39d8ca40f96f0ca9398b92b4b00342dd498c61ad
rkn2 <nap@psu.edu>
2023-09-05 15:34:02 -0400

    all working

--- readCSV6.py
+++ readCSV6.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 
 # Define the path to the CSV file containing data
 csv_file_path = '2023_9_5_output_blocks_cont3.csv'
+#num feature columns
+featCols = 18
 
 # Read the CSV file and extract the data
 data = []
@@ -16,14 +18,14 @@ with open(csv_file_path, 'r') as csv_file:
         names.append(name)  # Store the names in the same order as the data
         # Extract and convert points from CSV to tuples
         points = []
-        for point_str in row[1:-3]:
+        for point_str in row[1:-featCols]:
             if point_str.strip() == '':
                 continue  # Skip empty points
             point = tuple(map(float, point_str.split(',')))
             points.append(point)
         # Handle discrete and continuous values, converting to appropriate types
         features = []
-        for feature in row[-3:]:
+        for feature in row[-featCols:]:
             if not feature:
                 # Replace empty entries with '-'
                 feature = '-'
@@ -64,7 +66,7 @@ color_mapping = {
 font = ImageFont.load_default()  # You can choose an appropriate font
 
 # Create images for each feature column
-for feature_index, feature_name in enumerate(header[-3:], start=len(data[0][0]) + 1): #HER
+for feature_index, feature_name in enumerate(header[-featCols:], start=len(data[0][0]) + 1): #HER
     output_image_path = f'output_image_{feature_name}.png'
     image = Image.new('RGB', (image_width, image_height), 'white')  # Create an image based on calculated dimensions
     draw = ImageDraw.Draw(image)  # Create a drawing object for the image
